cc58e71d4bfe296ce656f1850f1f9887d8e78d5f
fhw <877172774@qq.com>
2017-08-31 13:26:59 +0800

    fhw

--- test.php
+++ test.php
@@ -1,7 +1,6 @@
 <?php
 
-    $a = 'Hello';
-    $b = 'Workd';
-    $c = '!';
-
-    echo $a . " " . $b . $c . "\n";
+    $arr = [50,10,90,30,70,40,80,60,20];
+    echo implode($arr, ' ') . "\n";
+    sort($arr);
+    echo implode($arr, ' ') . "\n";
